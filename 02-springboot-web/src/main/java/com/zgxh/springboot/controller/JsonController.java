101908449fa8c69d979679a8ea25409c8fde7eaa
Zgxh <1227814546@qq.com>
2020-02-29 15:29:11 +0800

    Jackson进行反序列化（树方式、DataBind方式），集合的反序列化2种方式

--- 02-springboot-web/src/main/java/com/zgxh/springboot/controller/JsonController.java
+++ 02-springboot-web/src/main/java/com/zgxh/springboot/controller/JsonController.java
@@ -1,10 +1,17 @@
 package com.zgxh.springboot.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.zgxh.springboot.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -43,5 +50,47 @@ public class JsonController {
         Map map = new HashMap();
         map.put("time", new Date());
         return map;
+    }
+
+    /* 下面用Jackson进行序列化和反序列化 */
+    @Autowired
+    ObjectMapper objectMapper; // ObjectMapper 是Jackson进行序列化和反序列化的工具
+
+    // 树遍历的方式
+    @GetMapping(value = "/tree.json")
+    public String readTree() throws JsonProcessingException, IOException {
+        String json = "{\"name\":\"zgxh\",\"id\":\"122\"}"; // 模拟传入的JSON字符串
+        JsonNode node = objectMapper.readTree(json); // 返回JsonNode作为根节点
+        String name = node.get("name").asText();
+        int id = node.get("id").asInt();
+        return "name:" + name + ", id:" + id;
+    }
+
+    // 采用DataBind方式
+    @GetMapping("/databind.json")
+    public String dataBind() throws JsonProcessingException, IOException {
+        String json = "{\"name\":\"zgxh\",\"id\":\"122\"}"; // 模拟传入的JSON字符串
+        User user = objectMapper.readValue(json, User.class); // 使用readValue来反序列化JSON到POJO
+        // 把POJO序列化成JSON可以使用writeValueAsString
+        return "name:" + user.getName() + ", id:" + user.getId();
+    }
+
+    /* 集合的反序列化 */
+    @GetMapping(value = "/collection.json")
+    public String collection(@RequestBody List<User> list) { // 使用@RequestBody把JSON自动映射到方法参数，
+        // 泛型类型User会被编译到字节码中从而被识别为User类型，每个json体反序列化到一个User对象
+        StringBuilder sb = new StringBuilder();
+        for (User user : list) {
+            sb.append(user.getName()).append(" ");
+        }
+        return sb.toString();
+    }
+
+    // 另一种方式
+    @GetMapping(value = "/collection1.json")
+    public int collection1(@RequestBody String json) throws JsonProcessingException, IOException {
+        JavaType type = objectMapper.getTypeFactory().constructParametricType(List.class, User.class); // 使用JavaType来提供泛型信息
+        List<User> list = objectMapper.readValue(json, type); // 不能直接用List.class，因为泛型类型没有指定
+        return list.size();
     }
 }
