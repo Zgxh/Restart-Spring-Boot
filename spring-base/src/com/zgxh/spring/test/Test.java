ee8ce24586e6b0e6973f39795d3e73187b660f43
Zgxh <1227814546@qq.com>
2020-06-06 00:52:38 +0800

    [add] spring autowire, .xml context namespace

--- spring-base/src/com/zgxh/spring/test/Test.java
+++ spring-base/src/com/zgxh/spring/test/Test.java
@@ -1,6 +1,7 @@
 package com.zgxh.spring.test;
 
 import com.zgxh.spring.bean.Laboratory;
+import com.zgxh.spring.bean.Person;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -11,9 +12,15 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Test {
 
     public static void main(String[] args) {
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring-config.xml");
-        Laboratory laboratory = (Laboratory) applicationContext.getBean("laboratory");
-        Laboratory laboratory1 = (Laboratory) applicationContext.getBean("myFactoryBean");
-        System.out.println(laboratory);
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("conf/spring-config.xml");
+        // Laboratory laboratory = (Laboratory) applicationContext.getBean("laboratory");
+        // Laboratory laboratory1 = (Laboratory) applicationContext.getBean("myFactoryBean");
+        // System.out.println(laboratory);
+        Laboratory laboratory2 = applicationContext.getBean("laboratory2", Laboratory.class);
+        System.out.println(laboratory2);
+
+        // ApplicationContext applicationContext = new ClassPathXmlApplicationContext("conf/context-test.xml");
+        // Person person = applicationContext.getBean("person", Person.class);
+        // System.out.println(person);
     }
 }
